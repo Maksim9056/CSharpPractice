3419deec6c5a7199be9e4fd598821b4342f2ea0d
CBG\MBobretsov <bobreczovm@inbox.ru>
2022-07-19 10:23:01 +0300

    no message

---
+++ Person1.cs
@@ -0,0 +1,16 @@
+﻿namespace Math
+{
+    public class Person
+    {
+        public static void TryParse(double a, double b, double c)
+        {
+            
+            double sum;
+            double result;
+
+            sum = a + b + c;
+            result = sum / 3;
+            Console.WriteLine($"{result}");
+        }
+    }
+}
