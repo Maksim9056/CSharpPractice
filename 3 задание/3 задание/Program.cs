798bd0dabd8cbdf3c15983d557d02a7c8ac33051
CBG\MBobretsov <bobreczovm@inbox.ru>
2022-07-22 11:14:18 +0300

    no message

---
+++ 3 задание/3 задание/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _3_задание
+{
+    internal class Program
+    {
+        private static bool Main(string[] args)
+        {
+            string inputPlayer, Computer;
+            int randomnum;
+            string loop;
+            bool keepPlaying = true;
+            int камень = 0;
+            int ножницы = 0;
+            int бумага = 0;
+            for (inputPlayer = null; камень > ножницы; )
+             
+                {
+                
+            
+            
+            
+            
+            
+            
+            }
+
+
+
+
+            }
+                
+}
+
+            
+                
+
+
+           
+            }
+
+                    Random myRandomObject = new Random();
+                    randomnum = myRandomObject.Next(1, 4);
+                    Console.Write("To play: enter R for Rock, S for Scissors, P for Paper.");
+                    inputPlayer = Console.ReadLine();
+                    inputPlayer = inputPlayer.ToUpper();
+              
+                
+
+                        inputPlayer = Console.ReadLine();
+                         
+
+
+                     
+                
+           
+        }
+    }
+}
